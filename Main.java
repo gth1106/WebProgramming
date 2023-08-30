cb2a3698f20048f32113c98399d92fdc7f50a811
gth1106 <gth6627@naver.com>
2023-08-30 20:56:47 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package BackJoon_;
+import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            // 예외가 발생할 가능성이 있는 코드
+//        } catch (ExceptionType1 e) {
+//            // ExceptionType1 예외가 발생했을 때 실행할 코드
+//        } catch (ExceptionType2 e) {
+//            // ExceptionType2 예외가 발생했을 때 실행할 코드
+//        } finally {
+//            // 예외 발생 여부와 상관없이 무조건 실행되는 코드
+//        }
+
+
+
+
+
+   }
+}
