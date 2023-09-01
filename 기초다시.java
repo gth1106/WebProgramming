a04f2e83cb56e7972282ed0a4325974840d73286
gth1106 <gth6627@naver.com>
2023-09-01 20:45:28 +0900

    Create 기초다시.java

---
+++ 기초다시.java
@@ -0,0 +1,28 @@
+package BackJoon_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 기초다시 {
+    public static void main(String[] args) throws IOException {
+//        // read() 사용 예
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int singleChar = br.read();  // 단일 문자 읽기
+//        System.out.println((char) singleChar);  // 읽은 문자 출력
+//
+//// readLine() 사용 예
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String line = br.readLine();  // 한 줄 읽기
+//        System.out.println(line);  // 읽은 줄 출력
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str + "??!");
+
+
+
+
+    }
+}
